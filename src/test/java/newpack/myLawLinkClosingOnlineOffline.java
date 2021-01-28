c81cd98c9541f24e6ce2e691713c39bb69ccc1de
Santhosh Thekke Neetiath <santhosh.tn@gmail.com>
2021-01-28 11:14:35 +0530

    commiting the changes

--- src/test/java/newpack/myLawLinkClosingOnlineOffline.java
+++ src/test/java/newpack/myLawLinkClosingOnlineOffline.java
@@ -87,11 +87,11 @@ public class myLawLinkClosingOnlineOffline {
 
 				// BVT
 
-				String baseurl = excel.getData(4, 0, 0);
+				//String baseurl = excel.getData(4, 0, 0);
 
 				// UAT
 
-				 //String baseurl=excel.getData(4, 0, 1);
+				 String baseurl=excel.getData(4, 0, 1);
 
 				// Staging
 
@@ -180,7 +180,7 @@ public class myLawLinkClosingOnlineOffline {
 
 				// driver.findElement(By.name("requiredBy")).sendKeys("23/10/2020");
 
-				rblawlink.setrequiredby("23/10/2020");
+				rblawlink.setrequiredby("23/10/2021");
 
 				// driver.findElement(By.xpath("//*[@id=\"step3\"]/table/tbody/tr[3]/td[2]/input[2]")).click();
 
