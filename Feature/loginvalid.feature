31d7ec205a57420b55cffc5e36963a85f726fe10
santhosh1980 <santhosh.tn@gmail.com>
2022-03-15 19:08:29 +0530

    files committed from my personal laptop for maven project

---
+++ Feature/loginvalid.feature
@@ -0,0 +1,8 @@
+Feature: Login functionality of Application 
+Scenario: Valid login test 
+Given Open the Chrome and launch the application	
+When Enter valid Username and Password
+And Click Login button
+Then User successfully logged in
+
+#Corresponding steps in Stepdefinition 1 file
---
+++ src/test/java/TestRunner/Runner.java
@@ -0,0 +1,21 @@
+package TestRunner;
+
+import org.junit.runner.RunWith;
+
+//import cucumber.api.CucumberOptions;
+//import cucumber.api.junit.Cucumber;
+
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="Feature", glue= "StepDefinition3", tags= "@resetmultipledata",
+
+dryRun = false,
+monochrome = true,
+plugin={"html:target/cucumber-html-report", "json:target/cucumber.json","pretty:target/cucumber-pretty.txt",
+		"usage:target/cucumber-usage.json", "junit:target/cucumber-results.xml"}
+)
+public class Runner {
+
+}
