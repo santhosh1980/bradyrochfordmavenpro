373a574fee19cd06e5e670d884fb5e749b82b0b3
Santhosh <santhosh.tn@gmail.com>
2023-01-09 15:37:15 +0530

    video recording files committed

--- src/test/java/example/testngsample.java
+++ src/test/java/example/testngsample.java
@@ -7,18 +7,23 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import lib.ScreenRecorderUtil;
+
 public class testngsample {
 	
 	
 	@Test
-	public void verifyHomepageTitle() {
+	public void verifyHomepageTitle() throws Exception {
 	String baseUrl = "http://demo.guru99.com/test/newtours/";
     //String driverPath = "C:\\geckodriver.exe";
     //public WebDriver driver ; 
 	System.setProperty("webdriver.chrome.driver", "C:\\Users\\U35035\\eclipse-workspace\\chromedriver_win32\\chromedriver.exe");
-
+	
+	//starting vodeo capture
+	ScreenRecorderUtil.startRecord("verify video capture");
 	  
 	  for (int i=1;i<=5;i++) {
+		  
 		  
 		  System.out.println("launching chrome browser"); 
 	      
@@ -33,10 +38,12 @@ public class testngsample {
 	      driver.close();
 	      driver.quit();
 	      driverservice.stop();
+	      
 		  
 	  }
        
-      
+	//video capture ending  
+	  ScreenRecorderUtil.stopRecord();
   }
 
 }
